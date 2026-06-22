d7eefe3a8d817cf50930065c706bed7b1affbda5
Bhondeneha <bhonden@moodys.com>
2026-06-22 23:11:27 +0530

    Print for test logs

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
+from datetime import datetime, timezone
 
 app = FastAPI()
 
+# Runs when container starts
+print(f"QA-DEPLOY-LOG-TEST-{datetime.now(timezone.utc).isoformat()}", flush=True)
+
 @app.get("/ping")
 async def ping():
-	return {"message": "pong"}
+    return {"message": "pong"}
 
 if __name__ == "__main__":
-	uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
